b72ecba1a5290f1dd8ff5d1d4e876c416419c5ec
Art83 <shvetsov1983@gmail.com>
2022-05-27 10:07:05 +1000

    first attempt

--- cache.go
+++ cache.go
@@ -3,21 +3,39 @@ package cache
 import "time"
 
 type Cache struct {
+	values map[string]kv
+}
+
+type kv struct {
+	value    string
+	expiry   bool
+	deadline time.Time
 }
 
 func NewCache() Cache {
-	return Cache{}
+	var cache Cache
+	cache.values = make(map[string]kv)
+	return cache
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (c Cache) Get(key string) (string, bool) {
+	return c.values[key].value, c.values[key].expiry
 }
 
-func (receiver) Put(key, value string) {
+func (c Cache) Put(key, value string) {
+	c.values[key] = kv{value: value, expiry: false}
 }
 
-func (receiver) Keys() []string {
+func (c Cache) Keys() []string {
+	keys := make([]string, 0, len(c.values))
+	for k, v := range c.values {
+		if v.expiry == false || (v.expiry == true && v.deadline.After(time.Now())) {
+			keys = append(keys, k)
+		}
+	}
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c Cache) PutTill(key string, value string, deadline time.Time) {
+	c.values[key] = kv{value: value, expiry: true, deadline: deadline}
 }
